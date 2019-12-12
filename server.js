e73b2d8e59ee625e7b66bdd369e557fbfb0388fd
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-12 15:55:16 -0800

    seeds initially built / stretch started for future purposes

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const RecipesRouter = require('./recipes/recipes_router');
+const IngredientsRouter = require('./ingredients/ingredients_router');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/recipes', RecipesRouter);
+server.use('/api/ingredients', IngredientsRouter);
+
+module.exports = server;
